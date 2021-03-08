21ce7d47675148f82dbecef42b3991b124f1627d
jono1234321 <jonnyxbox410@hotmail.com>
2021-03-08 19:20:25 +1000

    started setting up auth

--- ProfileScreen/ProfileScreen.js
+++ ProfileScreen/ProfileScreen.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
-import { ScrollView, TextInput } from 'react-native';
+import { ScrollView, TextInput, TouchableOpacity } from 'react-native';
 import styles from "../styles.js"; 
 import TopNavBar from "../TopNavBar.js"; 
 import BottomNavBar from "../BottomNavBar.js"; 
@@ -9,16 +9,41 @@ export default function ProfileScreen() {
 
     const placeHolderColor = "#a6a6a6"; 
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+
     return (
-        <View style={{height: "100%"}}>
+        <View style={{height: "100%", color: "#fff"}}>
             <TopNavBar />
             <ScrollView
             contentContainerStyle={styles.workoutScreenContainer}
             showsVerticalScrollIndicator={false}>
 
                 <Text style={styles.header}>Profile</Text>
-                <Text style={{color: "white", fontSize: 15}}>Profile info goes here.</Text>
-            
+
+                <View style={styles.inputView}>
+                    <TextInput
+                        style={styles.TextInput}
+                        placeholder="Email."
+                        placeholderTextColor="#003f5c"
+                        onChangeText={(email) => setEmail(email)}
+                    />
+                </View>
+                
+                <View style={styles.inputView}>
+                    <TextInput
+                        style={styles.TextInput}
+                        placeholder="Password."
+                        placeholderTextColor="#003f5c"
+                        secureTextEntry={true}
+                        onChangeText={(password) => setPassword(password)}
+                    />
+                </View>
+
+                <TouchableOpacity>
+                    <Text style={styles.loginBtn}>Login</Text>
+                </TouchableOpacity>
+
             </ScrollView>
             <BottomNavBar />
         </View>
--- styles.js
+++ styles.js
@@ -123,5 +123,39 @@ export default StyleSheet.create({
       padding: 10, 
       height: 75, 
       width: "95%"
-    }
+    }, 
+
+    // Profile styles
+    inputView: {
+      backgroundColor: "#fff",
+      borderRadius: 30,
+      width: "70%",
+      height: 45,
+      marginBottom: 20,
+      alignItems: "center",
+    },
+    
+    TextInput: {
+      height: 50,
+      flex: 1,
+      padding: 10,
+      marginLeft: 20,
+    }, 
+
+    loginBtn: {
+      width: 150,
+      borderRadius: 25,
+      height: 30,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      backgroundColor: "#FF1493",
+      padding: 50
+    },
+
+    forgot_button: {
+      height: 30,
+      marginBottom: 30,
+    },
+
   });
